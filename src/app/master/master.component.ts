f7f85e368525d3c40e6454613bff89b42ee72126
Miroslav Stancic <miroslav.stancic@gmail.com>
2019-10-23 15:38:03 +0200

    handle admin content on master component

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router, Event, NavigationEnd} from '@angular/router';
+import {LoginService} from '../auth/services/login.service';
 
 @Component({
   selector: 'app-master',
@@ -7,14 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MasterComponent implements OnInit {
 
-  public opened: boolean = true;
-  constructor() { }
+  public adminRoute: boolean;
+  public opened = true;
+
+  constructor(private router: Router,
+              private loginService: LoginService) {
+  }
 
   ngOnInit() {
+    /* Initial route check */
+    this.isAdminRoute(this.router.url);
+    /* In case of route change */
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.isAdminRoute(event.url);
+      }
+    });
+  }
+
+  private isAdminRoute(url) {
+    this.adminRoute = url.startsWith('/admin');
   }
 
   toggleMenu() {
     this.opened = !this.opened;
+  }
+
+  loginLogout() {
+    if(this.adminRoute) {
+      this.loginService.logout();
+    } else {
+      this.router.navigate(['/auth']);
+    }
   }
 
 }
