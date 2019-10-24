e730d3fb59f3c5415b70fa87e6f6e8ec903689bd
Nemanja Stancic <neco_bl@yahoo.com>
2019-10-24 10:24:04 +0200

    add category list component

--- src/app/city-events-category/categories-list/categories-list.component.ts
+++ src/app/city-events-category/categories-list/categories-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource,  } from '@angular/material';
+import { CityEventCategory } from 'src/app/core/models/city-event-category.model';
 
 @Component({
   selector: 'app-categories-list',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories-list.component.css']
 })
 export class CategoriesListComponent implements OnInit {
+  
+  displayedColumns: string[] = ['categorie', 'edit' , 'delete'];
+  dataSource = new MatTableDataSource<CityEventCategory>();
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  loadCityEventCategory() {
+    this.dataSource.data = this.service.getEvents();
+  }
 }
